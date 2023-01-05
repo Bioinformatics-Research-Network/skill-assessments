278e3aa68cec0b46b896d1f47ea547c71ff31e15
kkwock <kmaekwock@gmail.com>
2023-01-04 18:25:04 -0800

    Update kkwock_tictactoe.R

--- R Programming/kkwock_tictactoe.R
+++ R Programming/kkwock_tictactoe.R
@@ -3,12 +3,12 @@
 
 # Function
 random.fill <- function(mat, choice){
-  empty.idx <- which(mat==0, mat)
+  empty.idx <- which(mat=='', mat)
   mat[sample(empty.idx, 1)] <- choice
   return(mat)
 }
 # Create an Empty Matrix
-mat <- matrix(0, nrow=3, ncol=3, byrow=T)
+mat <- matrix('', nrow=3, ncol=3, byrow=T)
 
 # Ask for User Input
 # Choose X or O
@@ -30,13 +30,55 @@ while(!(toupper(user.choice) %in% c("X", "O"))){
 npc.choice <- if(toupper(user.choice) == 'O') 'X' else 'O'
 
 if(user.choice=='O'){
-  print('O goes second.')
-  
-  # random first
-  mat <- random.fill(mat, npc.choice)
-}else if(user.choice=='X'){
-  
+  print('You go Second.')
+}else{
+  print('You go First.')
 }
 
-#rowsum to check
-# for diagonals, use identity matrix
+
+win = F
+
+while(win = F){
+  if(user.choice=='O'){
+    # random first
+    mat <- random.fill(mat, npc.choice)
+    mat
+    
+    # user fill 
+    rn <- as.numeric(readline(prompt="Which row?: "))
+    cn <- as.numeric(readline(prompt="Which column?: "))
+    mat[rn, cn] <- user.choice
+    mat
+    
+  }else if(user.choice=='X'){
+    # print board
+    mat 
+    
+    # user fill
+    rn <- as.numeric(readline(prompt="Which row?: "))
+    cn <- as.numeric(readline(prompt="Which column?: "))
+    mat[rn, cn] <- user.choice
+    mat
+    
+    # random second
+    mat <- random.fill(mat, npc.choice)
+    mat
+  }
+}
+
+# Check Win Function
+win.check <- function(x){
+  # win combinations
+  win.combos <- list(c(1,2,3), c(4,5,6), c(7,8,9), # Row
+                     c(1,4,7), c(2,5,8), c(3,7,9), # Col
+                     c(1,5,9), c(7,5,3)) # Diag
+  
+  val <- unlist(as.list(mat))
+  for(i in win.combos){
+    result <- unique(val[i]) == c("X", "O")
+    win = if(any(result == T)) T else F
+    
+  }
+  
+  return(result)
+}
